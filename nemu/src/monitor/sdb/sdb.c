7d47d289b26ce09e6c4d538958b4e3e014609e2b
tracer-ysyx <tracer@ysyx.org>
2025-01-11 15:43:58 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:43:58 up 33 min,  1 user,  load average: 0.19, 0.22, 0.22

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -92,7 +92,9 @@ static int cmd_printR(char *args){
 // 扫描内存
 static int cmd_x(char *args) {
   char *n = strtok(NULL," ");
+  printf("%s", n);
   char *vaddr = strtok(NULL," ");
+  printf("%s", vaddr);
   if(n == NULL || vaddr == NULL){
     printf("Usage: x N EXPR\r\n");
     return 0;
