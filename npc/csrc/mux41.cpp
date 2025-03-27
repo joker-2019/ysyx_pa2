1d291e3c0528072c86910ed3bff4475eebddc6c8
tracer-ysyx <tracer@ysyx.org>
2025-03-27 21:07:12 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:07:12 up  7:38,  1 user,  load average: 0.00, 0.00, 0.00

--- npc/csrc/mux41.cpp
+++ npc/csrc/mux41.cpp
@@ -1,14 +1,13 @@
 #include "verilated.h"
 #include "verilated_vcd_c.h"
-#include "Vmux41.h"
 #include <stdio.h>
 #include "nvboard.h"
 #include <stdlib.h>
 
 VerilatedContext* contextp = NULL;
 VerilatedVcdC* tfp = NULL;
-
 static Vmux41* top;
+void nvboard_bind_all_pins(Vmux41* top);
 
 void step_and_dump_wave(){
   top->eval();
@@ -28,32 +27,22 @@ void sim_exit(){
   step_and_dump_wave();
   tfp->close();
 }
-ioid sim_init(){
-  contextp = new VerilatedContext;
-  tfp = new VerilatedVcdC;
-  top = new Vmux41;
-  contextp->traceEverOn(true);
-  top->trace(tfp, 0);
-  tfp->open("dump.vcd");
-}
-
-void sim_exit(){
-  step_and_dump_wave();
-  tfp->close();
-}
 int main(){
+  top = new Vmux41;
   sim_init();
+  nvboard_bind_all_pins(top);
+  nvboard_init();
+  top->Y=0b00;  top->X0=0b11;  top->eval(); step_and_dump_wave();  // 运行一个时间步长并记录波形
+  	        top->X0=0b00;  top->eval(); step_and_dump_wave();  //00
+		
+  top->Y=0b01;  top->X1=0b10;  top->eval(); step_and_dump_wave();
+                top->X1=0b01;  top->eval(); step_and_dump_wave();  //01
 
-  top->Y=0b00;  top->X0=0b11;  step_and_dump_wave();  // 运行一个时间步长并记录波形
-  	        top->X0=0b00;  step_and_dump_wave();  //00
+  top->Y=0b10;  top->X2=0b11;  top->eval(); step_and_dump_wave();
+                top->X2=0b10;  top->eval(); step_and_dump_wave();  //10
 
-  top->Y=0b01;  top->X1=0b10;  step_and_dump_wave();
-                top->X1=0b01;  step_and_dump_wave();  //01
-
-  top->Y=0b10;  top->X2=0b11;  step_and_dump_wave();
-                top->X2=0b10;  step_and_dump_wave();  //10
-
-  top->Y=0b11;  top->X3=0b00;  step_and_dump_wave();  
-                top->X3=0b11;  step_and_dump_wave();  //11
+  top->Y=0b11;  top->X3=0b00;  top->eval(); step_and_dump_wave();  
+                top->X3=0b11;  top->eval(); step_and_dump_wave();  //11
   sim_exit(); 
+  nvboard_quit();
 }
