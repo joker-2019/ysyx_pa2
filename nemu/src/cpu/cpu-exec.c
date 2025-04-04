d230a64181bced3d3b56bb26a0638bd23982d949
tracer-ysyx <tracer@ysyx.org>
2025-04-04 23:49:23 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:49:23 up 14:27,  1 user,  load average: 0.23, 0.08, 0.02

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -39,7 +39,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 #ifdef CONFIG_WATCHPOINT
-  if(scan_all_wp()&& nemu_state.state!=NEMU_ABORT && nemu_state.state!=NEMU_END) {
+  if(scan_all_wp()) {
   printf("You triggered the watchpoint!\n");
   nemu_state.state = NEMU_STOP;
   }
