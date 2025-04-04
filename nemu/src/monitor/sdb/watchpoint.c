844380b2f93c329e4997ef0f57e9c2ba60f7d61f
tracer-ysyx <tracer@ysyx.org>
2025-04-05 00:36:50 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:36:50 up 15:15,  1 user,  load average: 0.02, 0.02, 0.00

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -140,6 +140,7 @@ bool scan_all_wp(){
   {
     bool success;
     word_t new_value = expr(temp->expr,&success);
+    printf("new_value = 0x%08x\n", new_value);  // 32位补零显示，带0x前缀
     if(temp->old_value != new_value){
       printf("%d watchpoint expression has changed: %s",temp->NO, temp->expr);
       printf("%d old_value is %u",temp->NO, temp->old_value);
