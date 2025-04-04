1a77a7a9e6ae1a558655b4f1adeae9777e145e42
tracer-ysyx <tracer@ysyx.org>
2025-04-04 22:09:00 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:09:00 up 12:47,  1 user,  load average: 0.20, 0.15, 0.09

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -334,7 +334,7 @@ Result eval(int p, int q){
     }
     else if(tokens[p].type == TK_REG){
       bool success_reg = false;
-      result.data = isa_reg_str2val(tokens[p].str, &success_reg);  //reg
+      result.data = isa_reg_str2val(tokens[p].str+1, &success_reg);  //reg
       result.is_valid = success_reg;
       if(success_reg == false)
       {
