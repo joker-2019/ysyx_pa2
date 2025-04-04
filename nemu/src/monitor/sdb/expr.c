42163d58bbe31eb1bf86543f6dfce569907aeaaa
tracer-ysyx <tracer@ysyx.org>
2025-04-05 00:12:12 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:12:12 up 14:50,  1 user,  load average: 0.03, 0.06, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -340,7 +340,8 @@ Result eval(int p, int q){
       // printf("tokens[p].str:%s\n", tokens[p].str);
       // printf("tokens[p].str+1:%s\n", tokens[p].str+1);
       result.data = isa_reg_str2val(tokens[p].str, &success_reg);  //reg
-      printf("result.data:%u",result.data);
+      //printf(result.data);
+      //printf("\n");
       result.is_valid = success_reg;
       if(success_reg == false)
       {
