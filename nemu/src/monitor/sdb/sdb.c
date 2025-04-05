860d26f4eb99ae34b3b287669cb31a31db49600f
tracer-ysyx <tracer@ysyx.org>
2025-04-05 10:03:26 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:03:26 up  1:40,  1 user,  load average: 0.33, 0.12, 0.09

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -296,7 +296,7 @@ void init_sdb() {
   /* Compile the regular expressions. */
   init_regex();
   /* test math expression calculation */
-  //test_expr();
+  test_expr();
   /* Initialize the watchpoint pool. */
   init_wp_pool();
 }
