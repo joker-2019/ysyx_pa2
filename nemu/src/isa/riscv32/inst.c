e1e7ae839f644e1b40d74b9b2de83a2e6bae5565
tracer-ysyx <tracer@ysyx.org>
2025-04-20 19:15:38 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:15:38 up 49 min,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -54,8 +54,8 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_N: immN(); break;
     case TYPE_J: immJ(); break;
-    case TYPE_B: src1R(); src2R(); immR(); break;
-    case TYPE_R: src1R(); src2R(); immB(); break;
+    case TYPE_B: src1R(); src2R(); immB(); break;
+    case TYPE_R: src1R(); src2R(); immR(); break;
     //TODO
   }
 }
