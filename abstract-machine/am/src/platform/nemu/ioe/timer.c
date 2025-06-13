2a84f2ecaf69ad1836980bb374123093a7590571
tracer-ysyx <tracer@ysyx.org>
2025-06-13 17:05:32 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:05:32 up  7:09,  0 users,  load average: 0.00, 0.05, 0.02

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,28 +5,13 @@
 //static uint64_t boot_time;
 
 void __am_timer_init() {
-  /*
-    // 获取当前时间，单位为毫秒
-  uint32_t low = inl(RTC_ADDR);     // 低32位
-  uint32_t high = inl(RTC_ADDR + 4); // 高32位
-  // 将当前初始化时间转换为64位整数
-  boot_time = ((uint64_t)high << 32) | low;
-*/
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-
  // 获取当前时间，单位为毫秒
   uint32_t low = inl(RTC_ADDR);     // 低32位
   uint32_t high = inl(RTC_ADDR + 4); // 高32位
   uptime->us = ((uint64_t)high << 32) | low;
-  // 计算从启动到现在的时间差（毫秒）
-  //uint64_t elapsed_ms = current_time - boot_time;
-
-  // 计算自系统启动以来的微秒数
-  //uptime->us = elapsed_ms * 1000; // 转换为微秒
-  //uptime->us = current_time;
-
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
