6ca0e8eba15354d009ae2c422fd68c12cd60459c
tracer-ysyx <tracer@ysyx.org>
2025-06-13 16:58:25 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:58:25 up  7:02,  0 users,  load average: 0.23, 0.07, 0.02

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,41 +1,40 @@
 #include <am.h>
 #include <nemu.h>
-
+#include <time.h>
 //系统启动时间，用于计算相对时间
 static uint64_t boot_time;
-static int is_initialized = 0;
+
 void __am_timer_init() {
     // 获取当前时间，单位为毫秒
   uint32_t low = inl(RTC_ADDR);     // 低32位
   uint32_t high = inl(RTC_ADDR + 4); // 高32位
   // 将当前初始化时间转换为64位整数
   boot_time = ((uint64_t)high << 32) | low;
-  is_initialized = 1;
+
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  // 获取系统启动时间
-  if(!is_initialized) {
-      __am_timer_init();
-  }
+
  // 获取当前时间，单位为毫秒
   uint32_t low = inl(RTC_ADDR);     // 低32位
   uint32_t high = inl(RTC_ADDR + 4); // 高32位
-  uint64_t current_time = ((uint64_t)high << 32) | low;
-
+  uptime->us = ((uint64_t)high << 32) | low;
   // 计算从启动到现在的时间差（毫秒）
-  uint64_t elapsed_ms = current_time - boot_time;
+  //uint64_t elapsed_ms = current_time - boot_time;
 
   // 计算自系统启动以来的微秒数
-  uptime->us = elapsed_ms * 1000; // 转换为微秒
+  //uptime->us = elapsed_ms * 1000; // 转换为微秒
+  //uptime->us = current_time;
 
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
+  // 填充 AM_TIMER_RTC_T 结构体
   rtc->second = 0;
   rtc->minute = 0;
   rtc->hour   = 0;
   rtc->day    = 0;
   rtc->month  = 0;
-  rtc->year   = 1900;
+  rtc->year   = 1990;
 }
+
