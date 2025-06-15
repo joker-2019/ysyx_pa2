e21089325c8ec7102ebca4fe7fcdb8fd83244e82
tracer-ysyx <tracer@ysyx.org>
2025-06-15 18:12:08 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:12:08 up  2:39,  0 users,  load average: 0.03, 0.02, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,10 +5,8 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
 
-    uint32_t key_code = inl(0x60);
+    uint32_t key_code = inl(KBD_ADDR);
     // 设置键盘状态
     kbd->keydown = (key_code & KEYDOWN_MASK) ? 1 : 0;  // 检查按键是否被按下
     kbd->keycode = key_code & ~KEYDOWN_MASK;  // 获取按键的断码, 去掉KEYDOWN_MASK位
-
-  
 }
