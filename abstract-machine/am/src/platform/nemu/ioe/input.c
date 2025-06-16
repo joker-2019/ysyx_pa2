28e78f75dc757fcb9ac64efd33d138c0ba55e7fc
tracer-ysyx <tracer@ysyx.org>
2025-06-16 11:45:46 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:45:46 up  1:31,  1 user,  load average: 0.00, 0.09, 0.12

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -2,11 +2,19 @@
 #include <nemu.h>
 
 #define KEYDOWN_MASK 0x8000
+static uint32_t key_state = 0; // 跟踪所有按键状态
+#define KEY_MASK(key) (1 << (key & 0x1F)) // 为每个键码分配一个位
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-
+    // 从键盘端口读取按键状态
     uint32_t key_code = inl(KBD_ADDR);
     // 设置键盘状态
     kbd->keydown = (key_code & KEYDOWN_MASK) ? 1 : 0;  // 检查按键是否被按下
     kbd->keycode = key_code & ~KEYDOWN_MASK;  // 获取按键的断码, 去掉KEYDOWN_MASK位
+    // 更新全局按键状态
+    if (kbd->keydown) {
+        key_state |= KEY_MASK(kbd->keycode);  // 设置按键状态为按下
+    } else {
+        key_state &= ~KEY_MASK(kbd->keycode); // 清除按键状态
+    }
 }
