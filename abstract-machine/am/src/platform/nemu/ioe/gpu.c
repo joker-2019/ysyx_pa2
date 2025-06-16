a484219d482d82d9eba13341d5364ecbde138bd7
tracer-ysyx <tracer@ysyx.org>
2025-06-16 19:22:49 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:22:49 up  9:08,  1 user,  load average: 0.00, 0.00, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,17 +4,29 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  //test code to fill the framebuffer with a pattern
+  int i;
+  int w = 0;  // TODO: get the correct width
+  int h = 0;  // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  //从vgactl寄存器中读取显示设备的配置.
+  uint32_t vga = inl(VGACTL_ADDR);
+  uint32_t W = (vga >> 16) & 0xFFFF; // 获取宽度
+  uint32_t H = vga & 0xFFFF;         // 获取高度
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = W, .height = H,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  //若sync为true, 则马上将帧缓冲中的内容同步到屏幕上.
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
