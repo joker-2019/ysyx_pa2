51011c54ea08906e9fd92f44e86fcd4d5846bf56
tracer-ysyx <tracer@ysyx.org>
2025-06-17 22:18:28 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:18:28 up 12:20,  1 user,  load average: 0.08, 0.06, 0.01

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -39,19 +39,19 @@
       printf("Audio callback called with len: %d, available samples: %d\n", len, audio_base[reg_count]);
       int nread = len < audio_base[reg_count] ? len : audio_base[reg_count]; // Read up to 'len' bytes from the sample buffer
       if (nread > CONFIG_SB_SIZE) nread = CONFIG_SB_SIZE; // 确保读取的样本不超过缓冲区大小
-
       memcpy(stream, sbuf, nread);  // 将 nread 字节的样本从 sbuf 复制到输出流 stream
 
       // 剩余有效样本数量
       int remain = audio_base[reg_count] - nread;
       // 将剩余样本向前移动
       memmove(sbuf, sbuf + nread, remain);
+      memset(sbuf + remain, 0, CONFIG_SB_SIZE - remain);
+      audio_base[reg_count] = remain;
       //memmove(sbuf, sbuf + nread, CONFIG_SB_SIZE - nread); // 使用 memmove 函数将剩余样本向前移动
       // 清零缓冲区尾部，避免残留旧数据
       //memset(sbuf + audio_base[reg_count] - nread, 0, nread);
       // 清零剩余空间，避免残留脏数据导致杂音
-      memset(sbuf + remain, 0, CONFIG_SB_SIZE - remain);
-      audio_base[reg_count] = remain;
+
       // audio_base[reg_count] -= nread; // 更新可用样本数量
       if(len > nread){
         printf("Audio callback: not enough samples, filling with silence\n");
