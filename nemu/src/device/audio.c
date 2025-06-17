6b2ea4c47d5cb78761cf2b0adf36b9f873787a63
tracer-ysyx <tracer@ysyx.org>
2025-06-17 22:19:55 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:19:55 up 12:22,  1 user,  load average: 0.53, 0.18, 0.05

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -50,7 +50,7 @@
 
       memmove(sbuf, sbuf + nread, CONFIG_SB_SIZE - nread); // 使用 memmove 函数将剩余样本向前移动
       // 清零缓冲区尾部，避免残留旧数据
-      memset(sbuf + audio_base[reg_count] - nread, 0, nread);
+      //memset(sbuf + audio_base[reg_count] - nread, 0, nread);
       // 清零剩余空间，避免残留脏数据导致杂音
       audio_base[reg_count] -= nread; // 更新可用样本数量
       if(len > nread){
