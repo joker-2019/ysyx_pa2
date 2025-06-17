5d53e07cdafdcbe0608764db51c792dd39d60c42
tracer-ysyx <tracer@ysyx.org>
2025-06-17 22:12:23 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:12:23 up 12:14,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -31,11 +31,11 @@
   static uint32_t *audio_base = NULL; // Base address for audio registers
   static SDL_AudioDeviceID dev = 0; // SDL audio device ID
   static SDL_AudioSpec obtained; // Audio specifications
-  static int audio_read_pos = 0; // Position in the sample buffer for reading
+  // static int audio_read_pos = 0; // Position in the sample buffer for reading
 
   // Audio callback function
   static void audio_callback(void *userdata, uint8_t *stream, int len) {
-    /* if(audio_base[reg_count] > 0) { // 如果有样本可用，则继续处理；否则直接返回（输出静音）
+     if(audio_base[reg_count] > 0) { // 如果有样本可用，则继续处理；否则直接返回（输出静音）
       printf("Audio callback called with len: %d, available samples: %d\n", len, audio_base[reg_count]);
       int nread = len < audio_base[reg_count] ? len : audio_base[reg_count]; // Read up to 'len' bytes from the sample buffer
       if (nread > CONFIG_SB_SIZE) nread = CONFIG_SB_SIZE; // 确保读取的样本不超过缓冲区大小
@@ -50,8 +50,8 @@
     } else {
       printf("Audio callback called with len: %d, but no samples available\n", len);
         memset(stream, 0, len);  // 没有样本时，全部填充为0
-    } */
-  uint32_t count = audio_base[reg_count];
+    } 
+/*   uint32_t count = audio_base[reg_count];
   printf("audio_callback called with len: %d, available samples: %d\n", len, count);
   uint32_t sbuf_size = audio_base[reg_sbuf_size];
   printf("sbuf_size: %d\n", sbuf_size);
@@ -68,7 +68,7 @@
     memset(stream + nread, 0, len - nread); // 剩余部分填0（静音）
   }
   // Debug log
-  printf("audio_callback: len = %d, nread = %d, remaining = %d\n", len, nread, audio_base[reg_count]);
+  printf("audio_callback: len = %d, nread = %d, remaining = %d\n", len, nread, audio_base[reg_count]); */
   }
 
   // Audio I/O handler
