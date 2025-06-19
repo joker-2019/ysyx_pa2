bb1d9fa6a7e0c2e2c8eb09eadfa4572f3e080593
tracer-ysyx <tracer@ysyx.org>
2025-06-19 14:50:38 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:50:38 up  4:08,  1 user,  load average: 0.11, 0.08, 0.02

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -36,7 +36,7 @@
   // Audio callback function
   static void audio_callback(void *userdata, uint8_t *stream, int len) {
      if(audio_base[reg_count] > 0) { // 如果有样本可用，则继续处理；否则直接返回（输出静音）
-      printf("Audio callback called with len: %d, available samples: %d\n", len, audio_base[reg_count]);
+      // printf("Audio callback called with len: %d, available samples: %d\n", len, audio_base[reg_count]);
       int nread = len < audio_base[reg_count] ? len : audio_base[reg_count]; // Read up to 'len' bytes from the sample buffer
       if (nread > CONFIG_SB_SIZE) nread = CONFIG_SB_SIZE; // 确保读取的样本不超过缓冲区大小
       memcpy(stream, sbuf, nread);  // 将 nread 字节的样本从 sbuf 复制到输出流 stream
@@ -46,11 +46,11 @@
       // 清零剩余空间，避免残留脏数据导致杂音
       audio_base[reg_count] -= nread; // 更新可用样本数量
       if(len > nread){
-        printf("Audio callback: not enough samples, filling with silence\n");
+        // printf("Audio callback: not enough samples, filling with silence\n");
         memset(stream + nread, 0, len - nread); // 如果复制的样本不足以填满整个缓冲区（len > nread），则剩余部分填充 0
       }
     } else {
-      printf("Audio callback called with len: %d, but no samples available\n", len);
+      // printf("Audio callback called with len: %d, but no samples available\n", len);
         memset(stream, 0, len);  // 没有样本时，全部填充为0
     } 
 }
