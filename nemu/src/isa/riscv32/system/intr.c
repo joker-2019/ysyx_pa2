5885c418b6c28b0369be6137b21e80ef5eb3d33d
tracer-ysyx <tracer@ysyx.org>
2025-08-23 01:59:46 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:59:46 up  1:35,  1 user,  load average: 0.01, 0.04, 0.00

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -21,9 +21,8 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    */
   cpu.sr.mepc = epc;  // 发生异常的指令地址
   cpu.sr.mcause = NO; // 异常号
-  // cpu.sr.mtval = 0; // 异常值
-
-  return cpu.sr.mtvec; //返回向量的入口
+    
+  return cpu.sr.mtvec; // 返回向量的入口(地址入口位置)
 }
 
 word_t isa_query_intr() {
