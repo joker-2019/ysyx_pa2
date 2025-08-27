0e38ac905dd77412e1129a34c5b9f378761736c4
tracer-ysyx <tracer@ysyx.org>
2025-08-27 21:22:23 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:22:23 up  2:59,  0 users,  load average: 0.08, 0.07, 0.02

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -32,6 +32,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
+  return NULL;
   printf("Context boosting!\n");
   printf("Context Size: %d\n", CONTEXT_SIZE);
   Context *ctx = (Context *)(kstack.end - CONTEXT_SIZE);
