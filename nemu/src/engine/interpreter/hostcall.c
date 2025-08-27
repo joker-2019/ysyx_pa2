b0007cfe348851433c039126b38a4d340c2b50f3
tracer-ysyx <tracer@ysyx.org>
2025-08-27 21:26:05 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:26:05 up  3:03,  0 users,  load average: 0.08, 0.14, 0.07

--- nemu/src/engine/interpreter/hostcall.c
+++ nemu/src/engine/interpreter/hostcall.c
@@ -57,7 +57,7 @@ word_t csr_read(uint32_t csr_addr) {
     // case 0xc: return cpu.sr.mtvec;
     case 0x305: return cpu.sr.mtvec;   // mtvec 地址 0x305（异常向量表基地址）
     case 0x300:
-      // printf("Reading mstatus CSR (0x300): 0x%x\n", cpu.sr.mstatus);
+      printf("Reading mstatus CSR (0x300): 0x%x\n", cpu.sr.mstatus);
       return cpu.sr.mstatus;          // 机器态状态寄存器
     case 0x341: return cpu.sr.mepc;   // mepc 的 CSR 地址为 0x341 保存异常发生时的 PC
     case 0x342: return cpu.sr.mcause; // mcause 的 CSR 地址为 0x342 异常原因码
