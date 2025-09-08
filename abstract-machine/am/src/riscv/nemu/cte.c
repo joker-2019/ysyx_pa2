82c7efca2c5e13fae64b01c4476f139f76281e38
tracer-ysyx <tracer@ysyx.org>
2025-09-08 20:08:59 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:08:59 up  1:26,  0 users,  load average: 0.11, 0.07, 0.01

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -32,7 +32,7 @@ Context* __am_irq_handle(Context *c) {
 extern void __am_asm_trap(void);
 
 bool cte_init(Context*(*handler)(Event, Context*)) {
-  printf("cte_init booting!\n");
+  // printf("cte_init booting!\n");
   // initialize exception entry
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
   
@@ -43,14 +43,14 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  printf("Context boosting!\n");
-  printf("Context Size: %d\n", CONTEXT_SIZE);
+  // printf("Context boosting!\n"); 
+  // printf("Context Size: %d\n", CONTEXT_SIZE);
   Context *ctx = (Context *)(kstack.end - CONTEXT_SIZE); //定义上下文结构体的大小
   ctx->mstatus = 0x1800;
   ctx->mepc = (uintptr_t)entry; // 异常入口地址
-  printf("ctx->mepc : %0x\n", ctx->mepc);
+  // printf("ctx->mepc : %0x\n", ctx->mepc);
   ctx->gpr[10] = (uintptr_t)arg; // a0寄存器（x10）用于传递函数参数arg（符合RISC-V调用约定）
-  printf("ctx->gpr[10] : %0x\n", ctx->gpr[10]);
+  // printf("ctx->gpr[10] : %0x\n", ctx->gpr[10]);
   ctx->gpr[2] = (uintptr_t)ctx; //sp 在汇编代码中被单独定义  addi sp, sp, -CONTEXT_SIZE  ; 将栈指针向下移动，预留保存上下文的空间
   return ctx;
 }
