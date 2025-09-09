2e4daac1a5c5c0c05a9661c94f012d7440980c5a
tracer-ysyx <tracer@ysyx.org>
2025-09-09 18:25:58 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:25:58 up  9:50,  0 users,  load average: 0.25, 0.06, 0.02

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -50,8 +50,6 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   ctx->mepc = (uintptr_t)entry; // 异常入口地址
   // printf("ctx->mepc : %0x\n", ctx->mepc);
   ctx->gpr[10] = (uintptr_t)arg; // a0寄存器（x10）用于传递函数参数arg（符合RISC-V调用约定）
-  printf("ctx->gpr[0] : 0x%08x\n", ctx->gpr[0]);
-  printf("ctx->gpr[5] : 0x%08x\n", ctx->gpr[5]);
   ctx->gpr[2] = (uintptr_t)ctx;//sp 在汇编代码中被单独定义  addi sp, sp, -CONTEXT_SIZE  ; 将栈指针向下移动，预留保存上下文的空间
   return ctx;
 }
