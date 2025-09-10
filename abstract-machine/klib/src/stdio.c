bb3a7a3e4d4280026a43454de683d8c9dd3a364b
tracer-ysyx <tracer@ysyx.org>
2025-09-10 11:28:19 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:28:19 up  2:54,  0 users,  load average: 0.08, 0.02, 0.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,7 +6,7 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 //将数字转换为字符串的辅助函数
-static void number_to_str(char *buf, int num) {
+/* static void number_to_str(char *buf, int num) {
     if (num == 0) {
         *buf++ = '0';
         *buf = '\0';
@@ -32,6 +32,36 @@ static void number_to_str(char *buf, int num) {
         *p++ = '-';
     }
 
+    while (i > 0) {
+        *p++ = temp[--i];
+    }
+    *p = '\0';
+} */
+
+// 支持有符号/无符号，10进制/16进制，int/long 的通用数字转字符串
+static void number_to_str(char *buf, unsigned long num, int base, int is_signed, int is_negative) {
+    char temp[32];
+    int i = 0;
+
+    if (num == 0) {
+        temp[i++] = '0';
+    } else {
+        while (num > 0) {
+            int digit = num % base;
+            temp[i++] = (digit < 10) ? ('0' + digit) : ('a' + digit - 10);
+            num /= base;
+        }
+    }
+
+    char *p = buf;
+    if (is_negative) {
+        *p++ = '-';
+    }
+    // 16进制前缀
+    if (base == 16) {
+        *p++ = '0';
+        *p++ = 'x';
+    }
     while (i > 0) {
         *p++ = temp[--i];
     }
@@ -63,15 +93,57 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
     while (*fmt) {
         if (*fmt == '%') {
             fmt++;
+            int long_flag = 0;
+            if (*fmt == 'l') { long_flag = 1; fmt++; }
             switch (*fmt) {
                 case 'd': {
-                    int num = va_arg(args_copy, int);
+                    /* int num = va_arg(args_copy, int);
                     char num_buf[32];
                     number_to_str(num_buf, num);
                     char *p = num_buf;
                     while (*p) {
                         *buf++ = *p++;
                         count++;
+                    }
+                    break; */
+                    if (long_flag) {
+                        long num = va_arg(ap, long);
+                        char num_buf[32];
+                        number_to_str(num_buf, (unsigned long)(num < 0 ? -num : num), 10, 1, num < 0);
+                        char *p = num_buf; while (*p) { *buf++ = *p++; count++; }
+                    } else {
+                        int num = va_arg(ap, int);
+                        char num_buf[32];
+                        number_to_str(num_buf, (unsigned int)(num < 0 ? -num : num), 10, 1, num < 0);
+                        char *p = num_buf; while (*p) { *buf++ = *p++; count++; }
+                    }
+                    break;
+                }
+                case 'u':{
+                    if (long_flag) {
+                        unsigned long num = va_arg(ap, unsigned long);
+                        char num_buf[32];
+                        number_to_str(num_buf, num, 10, 0, 0);
+                        char *p = num_buf; while (*p) { *buf++ = *p++; count++; }
+                    } else {
+                        unsigned int num = va_arg(ap, unsigned int);
+                        char num_buf[32];
+                        number_to_str(num_buf, num, 10, 0, 0);
+                        char *p = num_buf; while (*p) { *buf++ = *p++; count++; }
+                    }
+                    break;
+                }
+                case 'x': {
+                    if (long_flag) {
+                        unsigned long num = va_arg(ap, unsigned long);
+                        char num_buf[32];
+                        number_to_str(num_buf, num, 16, 0, 0);
+                        char *p = num_buf; while (*p) { *buf++ = *p++; count++; }
+                    } else {
+                        unsigned int num = va_arg(ap, unsigned int);
+                        char num_buf[32];
+                        number_to_str(num_buf, num, 16, 0, 0);
+                        char *p = num_buf; while (*p) { *buf++ = *p++; count++; }
                     }
                     break;
                 }
