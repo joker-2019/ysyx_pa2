0d7fe2157884eeb11a525b66e502f41771f2884d
tracer-ysyx <tracer@ysyx.org>
2025-09-21 19:14:19 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:14:19 up  7:53,  0 users,  load average: 0.00, 0.00, 0.00

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -1,6 +1,7 @@
 #include <am.h>
 #include <klib-macros.h>
-#include <stdio.h> 
+#include <stdio.h>
+#include <nemu.h> 
 
 extern char _heap_start;
 int main(const char *args);
@@ -16,6 +17,7 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[] = MAINARGS;
 
 void putch(char ch) {
+  outb(SERIAL_PORT, ch);
 }
 
 void halt(int code) {
