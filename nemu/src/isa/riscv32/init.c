d61a2980d4aebba5c0667f9615a96834cc27d10b
tracer-ysyx <tracer@ysyx.org>
2025-09-25 00:24:37 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:24:37 up  3:16,  0 users,  load average: 0.09, 0.04, 0.00

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -29,6 +29,7 @@ static const uint32_t img [] = {
 static void restart() {
   /* Set the initial program counter. */
   cpu.pc = RESET_VECTOR;
+  printf("nemu start pc is 0x%x", cpu.pc);
 
   /* The zero register is always 0. */
   cpu.gpr[0] = 0;
