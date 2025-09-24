967cf8314f319f3315872de9b56fcf2ce2bb693e
tracer-ysyx <tracer@ysyx.org>
2025-09-24 22:25:18 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:25:18 up  1:16,  0 users,  load average: 0.00, 0.02, 0.00

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -1,12 +1,6 @@
 #include <am.h>
 #include <klib-macros.h>
 #include <klib.h>
-// 线路状态寄存器中的发送保持寄存器为空标志
-// UART 硬件参数（需与实际硬件匹配）
-#define UART_BASE 0x10000000  // UART 基地址
-#define UART_TX   0x00        // 发送缓冲区寄存器偏移（8位）
-#define UART_LSR  0x05        // 线路状态寄存器偏移（8位）
-#define UART_LSR_THRE 0x20    // 发送保持寄存器为空标志（可发送新字符）
 
 extern char _heap_start;
 int main(const char *args);
@@ -22,16 +16,6 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[] = MAINARGS;
 
 void putch(char ch) {
-/* // 指向 UART 线路状态寄存器（读）和发送寄存器（写）
-  volatile uint8_t *uart_lsr = (volatile uint8_t *)(UART_BASE + UART_LSR);
-  volatile uint8_t *uart_tx  = (volatile uint8_t *)(UART_BASE + UART_TX);
-
-  // 等待发送缓冲区为空（THRE 位为 1）
-  while ((*uart_lsr & UART_LSR_THRE) == 0);
-
-  // 发送字符到 UART
-  *uart_tx = ch; */
-  // putch(ch);
 }
 
 void halt(int code) {
