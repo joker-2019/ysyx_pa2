f33e298133be475ec57f029436199944ed3fc3f9
tracer-ysyx <tracer@ysyx.org>
2025-10-13 00:05:43 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:05:43 up 12:40,  0 users,  load average: 0.10, 0.03, 0.01

--- npc/csrc/Memory/memory.cpp
+++ npc/csrc/Memory/memory.cpp
@@ -17,28 +17,20 @@ uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 static uint64_t boot_time = 0; //系统启动时间
 #define RTC_ADDR     0xa0000048  // real-time clock MMIO 地址
 
-// 获取系统当前时间（微秒）
-static uint64_t get_time_us() {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return (uint64_t)tv.tv_sec * 1000000 + tv.tv_usec;
-}
 
-// 初始化时钟（在仿真启动时调用）
-/* void init_clock() {
-    boot_time = get_time_us();
-}
- */
-// 获取从启动到现在的时间（微秒）
-/* uint64_t get_clock_time() {
-    return get_time_us() - boot_time;
-} */
-
-uint64_t get_clock_time() {
+uint64_t get_time_us() {
     struct timespec ts;
     // clock_gettime(CLOCK_MONOTONIC, &ts);
     clock_gettime(CLOCK_REALTIME, &ts);
     return (uint64_t)ts.tv_sec * 1000000ULL + ts.tv_nsec / 1000ULL;
+}
+
+// 获取从启动到现在的时间（微秒）
+uint64_t get_clock_time() {
+    if (boot_time == 0) {
+        boot_time = get_time_us(); // 第一次调用时记录启动时间
+    }
+    return get_time_us() - boot_time; // 返回从启动到现在的微秒差值
 }
 
 void init_mem(){
@@ -78,11 +70,19 @@ extern "C" uint32_t mem_read(int pc) {
 }
 
 extern "C" uint32_t lw_mem_read(int addr, int len) {
-    if ((addr & ~0x3u) == RTC_ADDR)
+    /* if ((addr & ~0x3u) == RTC_ADDR)
     {
         uint64_t now = get_clock_time();    // 当前时间 (us)
         return (uint32_t)(now & 0xffffffff); // 返回低32位（us）
-        // return (uint32_t)time(NULL); // 秒数
+    } */
+    printf("[MEM] lw addr=0x%x len=%u\n", addr, len);
+    uint64_t now = get_clock_time();                           // 当前时间 (us)
+    if (addr == RTC_ADDR){
+        printf("[RTC] read time = %u\n", now & 0xffffffff);
+        return (uint32_t)(now & 0xffffffff); // 返回低32位 (us)
+    } 
+    if (addr == RTC_ADDR + 4){ 
+        return (uint32_t)(now >> 32); // 返回高32位
     }
     // uint32_t offset = addr - CONFIG_MBASE; // 按照字节寻址(uint8_t)
     assert(addr >= CONFIG_MBASE && addr + len <= CONFIG_MBASE + CONFIG_MSIZE);
@@ -98,10 +98,11 @@ extern "C" uint32_t lw_mem_read(int addr, int len) {
 // 注意：RISC-V数据访问通常是按字（4字节）对齐
 extern "C" void sw_mem_write(int addr, int len, int data) {
     // uint32_t aligned_addr = addr & ~0x3u; // 对其地址
-    printf("[UART] addr=0x%x, data=0x%x, len=%u\n", addr, data, len);
+    // printf("[UART] addr=0x%x, data=0x%x, len=%u\n", addr, data, len);
     // 串口写入
-    if (addr >= SERIAL_ADDR  && addr <= SERIAL_ADDR + 4) {
-        printf("[UART] write char = '%c' (0x%02x)\n", data & 0xFF, data & 0xFF);
+    // if (addr == SERIAL_ADDR  && addr <= SERIAL_ADDR + 4) {
+    if (addr == SERIAL_ADDR) {
+        // printf("[UART] write char = '%c' (0x%02x)\n", data & 0xFF, data & 0xFF);
         putchar((char)(data & 0xFF)); 
         // fflush(stdout);  // 立即刷新输出
         return;
