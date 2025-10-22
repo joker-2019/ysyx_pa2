77891934fa04adc75ecabddb4dba593c5343eaa1
tracer-ysyx <tracer@ysyx.org>
2025-10-22 11:02:56 +0800

    > compile NEMU ysyx_22040000 wp Linux wp-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:02:56 up  2:05,  0 users,  load average: 0.18, 0.48, 0.55

--- nemu/src/device/serial.c
+++ nemu/src/device/serial.c
@@ -34,7 +34,6 @@ static void serial_io_handler(uint32_t offset, int len, bool is_write) {
     /* We bind the serial port with the host stderr in NEMU. */
     case CH_OFFSET:
       if (is_write) {
-      serial_putc('\n');
       serial_putc(serial_base[0]);
       }
       else panic("do not support read");
